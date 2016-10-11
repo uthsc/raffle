25495396cadd8fd7d9a4d2060bf7d9e9a024ee08
gpspake <gpspake@gmail.com>
2016-10-11 16:57:02 -0500

    inital working render

--- public/scripts/namelist.js
+++ public/scripts/namelist.js
@@ -7,7 +7,7 @@ var Contestant = React.createClass({
 	},
 
 	render: function() {
-		return <div class="contestant">{this.props.name}</div>;
+		return <div className="contestant">{this.props.name}</div>;
 	}
 });
 
@@ -15,21 +15,79 @@ var ContestantList = React.createClass({
 
 	render: function() {
 
-		var commentNodes = this.props.data.map(function(comment) {
+		var contestantNodes = this.props.contestants.map(function(contestant) {
 			return (
-				<Contestant name={comment.author} key={comment.id} />
+				<Contestant name={contestant.name} key={contestant.id} />
 			);
 		});
 
-
-
-		return <div class="contestant-list">{commentNodes}</div>;
+		return (
+			<div className="contestant-list">
+				<h2>Contestants</h2>
+				{contestantNodes}
+			</div>
+		);
 	}
 });
 
-var data = [
-	{id: 1, author: "Pete Hunt", text: "This is one comment"},
-	{id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
+var ChooseWinnerButton = React.createClass({
 
-ReactDOM.render(<ContestantList data={data} />, document.getElementById('content'));
+	handleClick: function(){
+		this.props.test.data.contestants[0] = {id:12, name:"bob"}; //just testing (use setState()
+		console.log(this.props.test);
+	},
+
+	render: function(){
+		return (
+			<button onClick={this.handleClick}>
+				Choose Winner
+			</button>
+		);
+	}
+});
+
+var Raffle = React.createClass({
+
+
+	render: function() {
+		return (
+			<div className="raffle">
+				<h1>Raffle</h1>
+				<ContestantList contestants={this.props.data.contestants} />
+				<WinnerList winners={this.props.data.winners} />
+				<ChooseWinnerButton test={this.props}/>
+			</div>
+		);
+	}
+});
+
+var WinnerList = React.createClass({
+	render: function() {
+
+		var winnerNodes = this.props.winners.map(function(winner) {
+			return (
+				<Contestant name={winner.name} key={winner.id} />
+			);
+		});
+
+		return (
+			<div className="winner-list">
+				<h2>Winners</h2>
+				{winnerNodes}
+			</div>
+		);
+	}
+});
+
+var data = {
+	winners: [
+		{id: 1, name: "Pete Winner"},
+		{id: 2, name: "Jordan Winner"}
+	],
+	contestants: [
+		{id: 3, name: "Pete Contestant"},
+		{id: 4, name: "Jordan Contestant"}
+	]
+};
+
+ReactDOM.render(<Raffle data={data} />, document.getElementById('content'));
